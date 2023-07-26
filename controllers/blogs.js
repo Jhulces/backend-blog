bca2b7e369b3df77532944c718f5d02320ffedbd
Jhulces <jcochoae@gmail.com>
2023-07-26 02:52:50 -0500

    Exercises 4.17 - 4.19 completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,51 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user',{username: 1, name: 1})
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid'})
+    }
     
+    const user = await User.findById(decodedToken.id)
+
     if (!body.title || !body.url) {
         return response.status(400).end()
     }
 
     const likes = body.likes !== undefined ? body.likes : 0
 
+    //const user = await User.findById(body.userId)
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
+        user: user.id,
         likes: likes
     })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
--- models/user.js
+++ models/user.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  name: String,
-  passwordHash: String
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ],
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: String,
+    passwordHash: String
 })
 
 userSchema.plugin(uniqueValidator)
